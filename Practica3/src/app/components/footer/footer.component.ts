b9ef88f4dc21167bcb2c1eb4a8b43626966320e4
Menbalt516 <159486641+Menbalt516@users.noreply.github.com>
2024-12-15 20:29:51 -0400

    first commit

---
+++ Practica3/src/app/components/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+// footer.component.ts
+@Component({
+  selector: 'app-footer',  // Asegúrate de que el selector sea 'app-footer'
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+  standalone: true
+})
+export class FooterComponent { }
---
+++ Practica3/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+// navbar.component.ts
+@Component({
+  selector: 'app-navbar',  // Asegúrate de que el selector sea 'app-navbar'
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  standalone: true
+})
+export class NavbarComponent { }
